1ffd170db5ffd447b2cbc7520e968e7cbe9f910e
Shreya Kurian <shreyakurian@Shreyas-MacBook-Pro.local>
2021-10-31 17:28:14 +0530

    Modified no news page

--- feedly/src/components/Filter/FilteredNews.jsx
+++ feedly/src/components/Filter/FilteredNews.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useLocation } from "react-router";
 import { Typography, Button, Tag } from "@bigbinary/neetoui/v2";
 import { Highlight } from "@bigbinary/neeto-icons";
@@ -9,16 +9,16 @@ import BulletCard from "../Landing/BulletCard";
 import { NewsContext } from "../../contexts/newsFeeder";
 import { MONTHS } from "./constants";
 import { CATEGORIES } from "../../contexts/constants";
-import axios from "axios";
+
 
 const FilteredNews = () => {
   const [filteredNews, setFilteredNews] = useState([]);
-  const [fetchData, setFetchData] = useState([]);
+  // const [fetchData, setFetchData] = useState([]);
   const [showWriteMoreModal, setShowWriteMoreModal] = useState(false);
   const [dateArticle, setDateArticle] = useState(new Date());
-  const categoriesData = useContext(NewsContext)
-  const [tags,setTags] =useState([])
-  const [isArchivedTag,setIsArchivedTag] = useState(false)
+  const categoriesData = useContext(NewsContext);
+  const [tags, setTags] = useState([]);
+  const [isArchivedTag, setIsArchivedTag] = useState(false);
 
   const getDate = () => {
     var day = dateArticle.getDate();
@@ -31,113 +31,66 @@ const FilteredNews = () => {
   const { filteredCategories, isArchived } = useLocation().state;
 
   const getFilteredNews = (isArchivedCond) => {
-
     var filtered = tags.map((category) => {
       var news = categoriesData.filter((element) => {
         return CATEGORIES[category] === element.category;
       });
-      return (news)
-
+      return news;
     });
 
-    console.log(categoriesData[4].data.length)
-    filtered=filtered.flat()
+    console.log(categoriesData[4].data.length);
+    filtered = filtered.flat();
 
-    if(isArchivedCond) {
+    if (isArchivedCond) {
       setFilteredNews(filtered);
+    } else if (!isArchivedCond) {
+      let isArchivedNews = [];
+      var filter = filtered.map((news1) => {
+        var newsAll = news1.data.filter((news2) => {
+          return news2.date.split(",")[0] === getDate();
+        });
+        isArchivedNews.push({ category: news1.category, data: newsAll });
+        return news1;
+      });
+
+      setFilteredNews(isArchivedNews);
     }
+  };
 
 
-  else if(!isArchivedCond){
-    let isArchivedNews=[]
-    var filter = filtered.map((news1) =>{
-          var newsAll = news1.data.filter((news2)=>{return news2.date.split(",")[0]===getDate()})
-          isArchivedNews.push({"category": news1.category,"data":newsAll})
-          return news1
-     })
-
-     setFilteredNews(isArchivedNews)
+  const getNewsLength = () => {
+    return filteredNews.every(data=>data.data.length===0)
   }
-  }
-
-
-  useEffect(()=>{
-    setTags(filteredCategories)
-    setIsArchivedTag(isArchived)
-  },[filteredCategories])
-
-
-
 
   useEffect(() => {
-   getFilteredNews(isArchivedTag)
-  },[isArchived,tags,isArchivedTag]);
+    setTags(filteredCategories);
+    setIsArchivedTag(isArchived);
+  }, [filteredCategories]);
 
+  useEffect(() => {
+    getFilteredNews(isArchivedTag);
+  }, [isArchived, tags, isArchivedTag]);
 
   const handleClose = (i) => {
-    var filtered = tags.filter((_,index) => index !== i);
-    setTags(filtered)
-    window.localStorage.setItem(
-      "filteredCategories",
-      JSON.stringify(filtered)
-    );
+    var filtered = tags.filter((_, index) => index !== i);
+    setTags(filtered);
+    window.localStorage.setItem("filteredCategories", JSON.stringify(filtered));
   };
-
 
   const handleArchivedClose = () => {
-    console.log("reached here")
-    window.localStorage.setItem(
-      "isArchived",
-      JSON.stringify(false)
-    );
+    console.log("reached here");
+    window.localStorage.setItem("isArchived", JSON.stringify(false));
 
-    setIsArchivedTag(false)
+    setIsArchivedTag(false);
   };
 
+  console.log("return");
 
-
-
-  console.log("return")
-
-  return filteredNews.length === 0 ? (
+  return(
     <>
-      <div className="flex justify-center py-16">
-        <div className="space-y-5 flex flex-col justify-center">
-          <img src={NoNews} alt="No News" className="" />
-          <Typography style="h3">No News Articles Found</Typography>
-          <Button
-            className="mx-auto"
-            iconPosition="left"
-            icon={() => <Highlight size={15} />}
-            label={
-              <Typography style="h4" className="ml-3">
-                Write to us
-              </Typography>
-            }
-            style="secondary"
-            onClick={() => setShowWriteMoreModal(true)}
-          />
-        </div>
-      </div>
-      {categoriesData.length != 0 && (
-        <BulletCard
-          category="all"
-          articleSet={categoriesData[0].data}
-          MainArticleId={0}
-          filter={true}
-          bulletLength = {null}
-        />
-      )}
-
-      {showWriteMoreModal && (
-        <>
-          <WriteMore setShowWriteMoreModal={setShowWriteMoreModal} />
-        </>
-      )}
-    </>
-  ) : (
     <div>
-<div>        {tags.map((category, i) => (
+        {console.log(filteredNews)}
+        {tags.map((category, i) => (
           <Tag
             key={i}
             className="mr-5 mt-5"
@@ -157,19 +110,58 @@ const FilteredNews = () => {
         ) : null}
       </div>
 
-      {filteredNews.length > 0 &&
+      {getNewsLength() ?
+      <>
+      <div className="flex justify-center py-16">
+      <div className="space-y-5 flex flex-col justify-center">
+        <img src={NoNews} alt="No News" className="" />
+        <Typography style="h3">No News Articles Found</Typography>
+        <Button
+          className="mx-auto"
+          iconPosition="left"
+          icon={() => <Highlight size={15} />}
+          label={
+            <Typography style="h4" className="ml-3">
+              Write to us
+            </Typography>
+          }
+          style="secondary"
+          onClick={() => setShowWriteMoreModal(true)}
+        />
+      </div>
+    </div>
+
+      <BulletCard
+        category="all"
+        articleSet={categoriesData[0].data}
+        MainArticleId={0}
+        filter={true}
+        bulletLength={null}
+      />
+
+
+    {showWriteMoreModal && (
+      <>
+        <WriteMore setShowWriteMoreModal={setShowWriteMoreModal} />
+      </>
+    )}</> :
+
+  <>
+  {filteredNews.length > 0 &&
         filteredNews.map((news, i) => (
           <div key={i}>
             <NewsCard
-              bulletLength = {news.data.length}
+              bulletLength={news.data.length}
               articleSet={news.data}
               category={news.category[0].toUpperCase() + news.category.slice(1)}
               filter={true}
             />
           </div>
         ))}
-    </div>
-  );
+  </>
+    }
+      </>
+  )
 };
 
 export default FilteredNews;
